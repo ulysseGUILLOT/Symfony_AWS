01deffab4c9e70afbfed0411cf6619c46bb794ca
ulysseGUILLOT <ulysseguillot@gmail.com>
2024-06-25 18:41:54 +0200

    generate inventory.ini file

--- deploy.sh
+++ deploy.sh
@@ -32,3 +32,12 @@ docker container run -it --rm \
 # Extraction dns et ip
 app_server_public_dns=($(grep -A $AWS_INSTANCE_NUMBER "app_server_public_dns =" tmp/info | tail -n $AWS_INSTANCE_NUMBER | awk -F '"' '{print $2}'))
 app_server_public_ip=($(grep -A $AWS_INSTANCE_NUMBER "app_server_public_ip =" tmp/info | tail -n $AWS_INSTANCE_NUMBER | awk -F '"' '{print $2}'))
+
+# Ecriture de l'inventaire Ansible (inventory.ini)
+echo "[managers]" > ansible/playbook/inventory.ini
+echo "manager ansible_host=${app_server_public_dns[0]} aws_ip=${app_server_public_ip[0]} ansible_user=ubuntu ansible_ssh_private_key_file=myKey.pem" >> ansible/playbook/inventory.ini
+
+echo "[workers]" >> ansible/playbook/inventory.ini
+for ((i=1; i<${#app_server_public_dns[@]}; i++)); do
+    echo "worker$i ansible_host=${app_server_public_dns[$i]} aws_ip=${app_server_public_ip[$i]} ansible_user=ubuntu ansible_ssh_private_key_file=myKey.pem" >> ansible/playbook/inventory.ini
+done
